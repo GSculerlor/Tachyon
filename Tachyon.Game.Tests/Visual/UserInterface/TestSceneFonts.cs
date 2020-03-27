2d5d2f26f0556572a6288abbab00cf120db6b159
Ganendra Afrasya <gsculerlor@hotmail.com>
2020-03-27 17:42:25 +0700

    Add visual test for font

---
+++ Tachyon.Game.Tests/Visual/UserInterface/TestSceneFonts.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using Tachyon.Game.Graphics;
+using Tachyon.Game.Graphics.Containers;
+using Tachyon.Game.Testing.Visual;
+
+namespace Tachyon.Game.Tests.Visual.UserInterface
+{
+    [TestFixture]
+    public class TestSceneFonts : TachyonGridTestScene
+    {
+        public TestSceneFonts() : base(4, 2)
+        {
+            Cell(0).Add(new TextTestContainer(FontWeight.Light, false));
+            Cell(1).Add(new TextTestContainer(FontWeight.Light, true));
+            
+            Cell(2).Add(new TextTestContainer(FontWeight.Regular, false));
+            Cell(3).Add(new TextTestContainer(FontWeight.Regular, true));
+            
+            Cell(4).Add(new TextTestContainer(FontWeight.SemiBold, false));
+            Cell(5).Add(new TextTestContainer(FontWeight.SemiBold, true));
+            
+            Cell(6).Add(new TextTestContainer(FontWeight.Bold, false));
+            Cell(7).Add(new TextTestContainer(FontWeight.Bold, true));
+        }
+
+        private class TextTestContainer : FillFlowContainer
+        {
+            public TextTestContainer(FontWeight fontWeight, bool shouldItalic, Typeface typeface = Typeface.Quicksand)
+            {
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+                RelativeSizeAxes = Axes.X;
+                AutoSizeAxes = Axes.Y;
+
+                Add(new TachyonTextFlowContainer(text =>
+                {
+                    text.Font = TachyonFont.GetFont(typeface, weight: fontWeight, italics: shouldItalic);
+                })
+                {
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Text = "The quick brown fox jumps over the lazy dog. 1234567890.\n" +
+                           "GSculerlor/Tachyon\n" +
+                           "ここにいつまでもいるつもり"
+                });
+            }
+        }
+    }
+}
---
+++ Tachyon.Game/Testing/Visual/TachyonGridTestScene.cs
@@ -0,0 +1,36 @@
+﻿using osu.Framework.Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace Tachyon.Game.Testing.Visual
+{
+    public abstract class TachyonGridTestScene : TachyonTestScene
+    {
+        private readonly Drawable[,] cells;
+
+        protected readonly int Rows;
+        protected readonly int Cols;
+        
+        protected TachyonGridTestScene(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+
+            GridContainer testContainer;
+            Add(testContainer = new GridContainer { RelativeSizeAxes = Axes.Both });
+
+            cells = new Drawable[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                    cells[r, c] = new Container { RelativeSizeAxes = Axes.Both };
+            }
+
+            testContainer.Content = cells.ToJagged();
+        }
+
+        protected Container Cell(int index) => (Container)cells[index / Cols, index % Cols];
+        protected Container Cell(int row, int col) => (Container)cells[row, col];
+    }
+}
